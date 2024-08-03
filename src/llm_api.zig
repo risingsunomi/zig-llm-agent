66560c2e76f43ea3c847fda50257ed00b1c7af05
risingsunomi <vincentcastro@gmail.com>
2024-08-03 14:28:38 -0800

    added chat completion api, response isnt working

--- src/llm_api.zig
+++ src/llm_api.zig
@@ -4,13 +4,32 @@
 
 const std = @import("std");
 
-// ollama_chat_resp
-pub const ollama_chat_resp = struct {
+// ollama_gen_resp
+// response from generate api
+pub const ollama_gen_resp = struct {
     model: []const u8,
     created_at: []const u8,
     response: []const u8,
     done: bool,
     context: []const u32,
+    total_duration: u64,
+    load_duration: u64,
+    prompt_eval_count: u32,
+    prompt_eval_duration: u64,
+    eval_count: u32,
+    eval_duration: u64,
+};
+
+// ollama_chat_resp
+// response from chat api
+pub const ollama_chat_resp = struct {
+    model: []const u8,
+    created_at: []const u8,
+    message: struct {
+        role: []const u8,
+        content: []const u8,
+    },
+    done: bool,
     total_duration: u64,
     load_duration: u64,
     prompt_eval_count: u32,
@@ -32,38 +51,87 @@ pub const ollama_chat_req = struct {
     stream: bool,
 };
 
+// ollama chat req with tools
+const ollama_chat_req_tool = struct {
+    model: []const u8,
+    messages: []chat_message,
+    stream: bool,
+    tools: []struct {
+        type: []const u8,
+        function: struct {
+            name: []const u8,
+            description: []const u8,
+            parameters: struct {
+                type: []const u8,
+                properties: struct {},
+                required: []const []const u8,
+            },
+        },
+    },
+};
+
 // ollama_gen_chat
 // reaches out to ollama generate chat api with prompt
 // returns llm response
 pub fn ollama_gen_chat(allocator: std.mem.Allocator, messages: [][]const u8) anyerror![]const u8 {
+
+    // load messages into array for chat messages in ollama chat req struct
+    var oc_req_msgs = std.ArrayList(chat_message).init(allocator);
+    defer oc_req_msgs.deinit();
+
+    for (messages) |message| {
+        // create a chat_message struct
+        const cmsg = chat_message{
+            .content = message,
+            .role = "user",
+        };
+
+        try oc_req_msgs.append(cmsg);
+    }
+
+    // build chat req struct
+    const oc_req: ollama_chat_req = .{
+        .model = "llama3.1",
+        .stream = false,
+        .messages = oc_req_msgs.items,
+    };
+
+    std.debug.print(
+        "\noc_req model: {s}\noc_req messages len: {d} ",
+        .{ oc_req.model, oc_req.messages.len },
+    );
+
+    // convert request to a json string
+    // will cause error if arraylist is passed and not arraylist.items
+    var json_oc_req = std.ArrayList(u8).init(allocator);
+    defer json_oc_req.deinit();
+    try std.json.stringify(
+        oc_req,
+        .{},
+        json_oc_req.writer(),
+    );
+
+    std.debug.print("\njson_oc_req: {s}", .{json_oc_req.items});
+
+    // fetch to ollama generate api
+
     // setup http client
     var client = std.http.Client{ .allocator = allocator };
     defer client.deinit();
 
     // setup fetch
-    var buf: [4096]u8 = undefined;
     const uri = try std.Uri.parse("http://192.168.0.5/api/chat");
-    const fetch_loc: std.http.Client.FetchOptions.Location = .{ .uri = uri };
+    const fetch_loc: std.http.Client.FetchOptions.Location = .{
+        .uri = uri,
+    };
     var fetch_dynamic_list = std.ArrayList(u8).init(allocator);
     defer fetch_dynamic_list.deinit();
 
-    const oc_req = ollama_chat_req{ .model = "llama3.1", .stream = false };
-    for (messages) |message| {
-        // create a chat_message struct
-        const cmsg = chat_message{ .content = message, .role = "user" };
-
-        // turn the array to a slice to append
-        oc_req.messages = &[0]chat_message;
-        oc_req.messages = oc_req.messages ++ cmsg;
-    }
-
-    std.debug.print("oc_req:\n{s}\n", .{oc_req});
-
-    // fetch to ollama generate api
+    var server_header_buf: [4096]u8 = undefined;
     const resp: std.http.Client.FetchResult = try client.fetch(.{
-        .payload = oc_req,
+        .payload = json_oc_req.items,
         .method = .POST,
-        .server_header_buffer = &buf,
+        .server_header_buffer = &server_header_buf,
         .location = fetch_loc,
         .headers = .{
             .content_type = .{ .override = "application/json" },
@@ -74,6 +142,11 @@ pub fn ollama_gen_chat(allocator: std.mem.Allocator, messages: [][]const u8) any
     // get reponse if status is ok
     if (resp.status == std.http.Status.ok) {
         const ollama_response = try allocator.dupe(u8, fetch_dynamic_list.items);
+
+        std.debug.print(
+            "\nollama_response: {s}",
+            .{ollama_response},
+        );
 
         // Parse JSON
         const json_parse = try std.json.parseFromSlice(
@@ -87,8 +160,10 @@ pub fn ollama_gen_chat(allocator: std.mem.Allocator, messages: [][]const u8) any
         const parsed_oresp = json_parse.value;
 
         // Duplicate the response string to ensure it outlives this function
-        const response_copy = try allocator.dupe(u8, parsed_oresp.response);
+        const response_copy = try allocator.dupe(u8, parsed_oresp.message.content);
         allocator.free(ollama_response);
+
+        // will add detecting functions here, need to add prompt for llm to use functions
 
         std.debug.print("Ollama Response\n{s}\n", .{response_copy});
         return response_copy;
--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
-const llm_api = @import("llm_api");
+const llm_api = @import("llm_api.zig");
 
 test "using ollama_gen" {
     // use general purpose allocator
     const allocator = std.heap.page_allocator;
-    const cmsgs = [][]u8{ "Hello!", "Can you explain how to use Zig for robotics?" };
-    const oresp = try llm_api.ollama_gen_chat(allocator, cmsgs);
+    var cmsgs = [_][]const u8{ "Hello!", "Can you explain how to use Zig for robotics?" };
+    const oresp = try llm_api.ollama_gen_chat(allocator, &cmsgs);
     std.debug.print("Ollama Response\n{s}\n", .{oresp});
 }
